3f9bca660c16789ef8608245e861e38cfe580557
Angel Pichardo <aahpichardo@gmail.com>
2023-11-22 09:50:57 -0600

    cambios
    cambios

---
+++ app/cruds.js
@@ -0,0 +1,16 @@
+//Reservaciones
+var reservaciones = [
+    {id: 0, nombre: "", numeroDePersonas: 2, fechaLlegada: "", fechaSalida: "", tipoHabitación: ""},
+];
+
+
+//Pedidos restaurante
+var restaurante = [
+    {id: 0, nombre: 2, fechaPedido: "", platillo: "", bebida: "", postre:""},
+];
+
+//Recursos humanos
+var empleados = [
+    {id: 0, nombreEmpleado: 2, puesto: "", direccion: "", telefono: ""},
+];
+
